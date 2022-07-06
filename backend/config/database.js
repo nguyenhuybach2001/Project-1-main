57acb52e635032424eb192d18a38a74abce158a6
nguyenhuybach2001 <bachbom27@gmail.com>
2022-07-06 08:15:54 +0700

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`MongoDB connected with server ${data.connection.host}`);
+    })
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDB;
